2d737bed8eaacba2a55b2072f6a60e94b6fbfee6
Brett Hicklin <hicklin.brett@gmail.com>
2023-02-13 21:04:59 -0500

    started trying to implement the embeded player, does not work

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,15 +52,17 @@
 
 
 
-
 // $.get(requestUrl)
-//
+// function getApi()
 $.get("https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyBZWqlEr2JTKjchBOxdTr5oH14E9telw2k&type=video&q=ferrari%official%trailer",function(data){
+   var videoId = data.items[0].id.videoId
    console.log(data) 
    console.log(data.items[0].id.videoId)
 })
-
-
-
+// //need to pass videoId to this function
+function playVideo(){
+<iframe width="420" height="315" src="https://www.youtube.com/embed/zyYgDtY2AMY"></iframe>
+}
+//${data.items[0.id]}
 //items[0].id.videoId
 // in order to support multi-word titles: create an array of user input, and use split() to find spaces, and replace any spaces in search with %
