a93adea5c1920d1e8b03d26a0df628ff76f2ab26
Brett Hicklin <hicklin.brett@gmail.com>
2023-02-15 18:57:39 -0500

    removed unused section of code that weren't relavant anymore

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,31 @@
 
-var requestUrl = 'https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyBZWqlEr2JTKjchBOxdTr5oH14E9telw2k&type=video&q=ferrari%official%trailer';
+// var requestUrl = 'https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyBZWqlEr2JTKjchBOxdTr5oH14E9telw2k&type=video&q=ferrari%official%trailer';
 
 
 
-function getApi(requestUrl) {
-  fetch(requestUrl)
-//     ,{
-//     headers:{
-//         "Access-Control-Allow-Origin": "*"
-//     }
-//   })
-    .then(function (response) {
-      console.log(response);
+// function getApi(requestUrl) {
+//   fetch(requestUrl)
+// //     ,{
+// //     headers:{
+// //         "Access-Control-Allow-Origin": "*"
+// //     }
+// //   })
+//     .then(function (response) {
+//       console.log(response);
     
-      return response.json();
+//       return response.json();
 
-  }).then(function (data) {
-    console.log(data);
+//   }).then(function (data) {
+//     console.log(data);
 
-    var video = `<iframe width="420" height="315" src="https://www.youtube.com/embed/zyYgDtY2AMY"></iframe>`
+//     var video = `<iframe width="420" height="315" src="https://www.youtube.com/embed/zyYgDtY2AMY"></iframe>`
 
-$("#video").append(video)
+// $("#video").append(video)
   
-  })
-}
+//   })
+// }
 
-getApi(requestUrl);
+// getApi(requestUrl);
 
 
 
@@ -73,23 +73,17 @@ getApi(requestUrl);
 
 
 // var requestUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyBZWqlEr2JTKjchBOxdTr5oH14E9telw2k&type=video&q=${userInput}`;
-// var userInput = grab input from the search bar of user
+// var userInput = $("#").val() grab input from the search bar of user
+// var userInputQuerySearch = userInput.replaceAll(' ', '+')
 
-// .replace with regex
-// var userInputString = ""
-// userInput.push(userInputString)
-// var userInputStringSplit = userInputString.split(" ");
-//if (userInputStringSplit.length > 1){
-    // for (var i = 0; i < userInputStringSplit.length; i++) {
-    //   userInputStringSplit[i] = userInputStringSplit[i] + "%";
-    // }
-    // maybe change variable name of userInputStringSplit and userInput. Need to decide which var to 
-    //use in HTTP request
-    //} else {userInputStringSplit
-//}
 
-$.get(requestUrl)
-function getApi()
+
+
+
+
+
+// $.get(requestUrl)
+// function getApi()
 $.get(
   "https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyBZWqlEr2JTKjchBOxdTr5oH14E9telw2k&type=video&q=ferrari%official%trailer",
   function (data) {
@@ -100,7 +94,7 @@ $.get(
 );
 // //need to pass videoId to this function
 // function playVideo(){
-    var video = `<iframe width="420" height="315" src="https://www.youtube.com/embed/zyYgDtY2AMY"></iframe>`
+    var video = `<iframe width="600" height="400" src="https://www.youtube.com/embed/zyYgDtY2AMY"></iframe>`
 
     $("#video").append(video)
 // }
