fe3480697acf6e69a53bb4322a19fbfc76cac00b
Brett Hicklin <hicklin.brett@gmail.com>
2023-02-19 18:59:44 -0500

    made search bar larger, added styling for result.html, reformatted some code in result.js. Moved the back button into the nav bar.

--- assets/js/result.js
+++ assets/js/result.js
@@ -1,23 +1,24 @@
 var userInput = localStorage.getItem("searchInput");
 
 function playVideo() {
-    
-    console.log(userInput)
-    var userInputQuerySearch = userInput.replaceAll(" ", "+");
-    var requestUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyBZWqlEr2JTKjchBOxdTr5oH14E9telw2k&type=video&q=${userInputQuerySearch}+official+trailer`;
-   
-    $.get(requestUrl, function (data) {
-      var videoId = data.items[0].id.videoId
-      var video = $("#youtubeVideo");
-      var videoLink = video.attr("src", `https://www.youtube.com/embed/${videoId}`);
-      console.log(videoId)
-      $("#video").append(video);
-        
-    });
-  }
-  playVideo();
+  console.log(userInput);
+  var userInputQuerySearch = userInput.replaceAll(" ", "+");
+  var requestUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyBZWqlEr2JTKjchBOxdTr5oH14E9telw2k&type=video&q=${userInputQuerySearch}+official+trailer`;
 
- var homeBtn = $("#searchAgain")
- homeBtn.click(function() {
-    window.location.href="./index.html"
- })
+  $.get(requestUrl, function (data) {
+    var videoId = data.items[0].id.videoId;
+    var video = $("#youtubeVideo");
+    var videoLink = video.attr(
+      "src",
+      `https://www.youtube.com/embed/${videoId}`
+    );
+    console.log(videoId);
+    $("#video").append(video);
+  });
+}
+playVideo();
+
+var homeBtn = $("#searchAgain");
+homeBtn.click(function () {
+  window.location.href = "./index.html";
+});
