318900c4e26fed875fcd905b007d2ababf69a816
Brett Hicklin <hicklin.brett@gmail.com>
2023-02-20 15:48:32 -0500

    commented code

--- assets/js/result.js
+++ assets/js/result.js
@@ -1,18 +1,19 @@
+//grabbing the search term from local storage
 var userInput = localStorage.getItem("searchInput");
-
+//runs the playVideo function which takes users input and displays movie trailer
 function playVideo() {
-    
-    console.log(userInput)
-    var userInputQuerySearch = userInput.replaceAll(" ", "+");
-    var requestUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyBZWqlEr2JTKjchBOxdTr5oH14E9telw2k&type=video&q=${userInputQuerySearch}+official+trailer`;
-   
-    $.get(requestUrl, function (data) {
-      var videoId = data.items[0].id.videoId
-      var video = $("#youtubeVideo");
-      var videoLink = video.attr("src", `https://www.youtube.com/embed/${videoId}`);
-      console.log(videoId)
-      $("#video").append(video);
-        
-    });
-  }
-  playVideo();
+  //replaces all spaces in the search term with "+" so that the API can use the term in its URL
+  var userInputQuerySearch = userInput.replaceAll(" ", "+");
+
+  //takes the now mutated userInput and adds "official+trailer" to the end in order to grab a movie trailer
+  var requestUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyBZWqlEr2JTKjchBOxdTr5oH14E9telw2k&type=video&q=${userInputQuerySearch}+official+trailer`;
+
+  //using jquery to get the data needed from the API's response, and appending it to the iframe player to display on the page
+  $.get(requestUrl, function (data) {
+    var videoId = data.items[0].id.videoId;
+    var video = $("#youtubeVideo");
+    video.attr("src", `https://www.youtube.com/embed/${videoId}`);
+    $("#video").append(video);
+  });
+}
+playVideo();
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,25 @@
 
 var searchButton = document.getElementById("searchButton");
-
+//shows and hides the pop up modal box to use a validation for the search box
 function popup(){
   $("#validation").css("visibility", "visible")
   $("#closeValidationBtn").click(function(){
     $("#validation").css("visibility", "hidden")
   })
 }
-
+//click event on the search button
 searchButton.addEventListener("click", function () {
   
  // we'll have to move the OMDB data retrieval function (all the commented out code) to inside the new result.js
   var searchBar = document.getElementById("searchBar");
   var searchTerm = searchBar.value;
+  //validation for the search box. Will run the popup function if those conditions are met
   if (searchTerm === "" || searchTerm === " "){
     popup()
   } else {
+    //added the search term to local storage so it can be accessed on the result.js file in order to run the playVideo function
   localStorage.setItem("searchInput", searchTerm)
+  //after the search button is clicked, it changes the window to the result.html file to display the results
   window.location.href="./result.html"
   // var url = "http://www.omdbapi.com/?t=" + searchTerm + "&apikey=16ec6f98";
   // var xhr = new XMLHttpRequest();
