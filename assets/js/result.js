67ce91dfd195fef91d69ac2ff882ac945c4ae26e
Brett Hicklin <hicklin.brett@gmail.com>
2023-02-22 20:58:49 -0500

    added local storage for getting movie title

--- assets/js/result.js
+++ assets/js/result.js
@@ -1,13 +1,14 @@
 //grabbing the search term from local storage
 var userInput = localStorage.getItem("searchInput");
+var OMDBMovieTitle = localStorage.getItem("movieTitle")
 //var OMDBMovieTitle = 
 //runs the playVideo function which takes users input and displays movie trailer
-function playVideo(movieTitle) {
+function playVideo(OMDBMovieTitle) {
   // ensures the trailer is visible, and the movie title modal is hidden
   $("#video").css("visibility", "visible");
   $("#OMDBValidation").css("visibility", "hidden");
   //replaces all spaces in the search term with "+" so that the API can use the term in its URL
-  var userInputQuerySearch = movieTitle.replaceAll(" ", "+");
+  var userInputQuerySearch = OMDBMovieTitle.replaceAll(" ", "+");
 
   //takes the now mutated userInput and adds "official+trailer" to the end in order to grab a movie trailer
   var requestUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyBZWqlEr2JTKjchBOxdTr5oH14E9telw2k&type=video&q=${userInputQuerySearch}+official+trailer`;
@@ -21,7 +22,7 @@ function playVideo(movieTitle) {
   });
 }
 //pass OMDBMovieTitle to make sure youtube searches the same movie the OMDB response comes back with
-playVideo(userInput);
+playVideo(OMDBMovieTitle);
 
 var homeBtn = $("#searchAgain");
 homeBtn.click(function () {
